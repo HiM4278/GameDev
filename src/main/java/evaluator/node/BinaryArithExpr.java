74cf27cbf0250591b6f6491e251b5f21ba78e228
pasakorn <pasakorn141@gmail.com>
2023-02-14 15:57:34 +0700

    BinaryArithExpr

--- src/main/java/evaluator/node/BinaryArithExpr.java
+++ src/main/java/evaluator/node/BinaryArithExpr.java
@@ -14,11 +14,23 @@ public class BinaryArithExpr implements Expression{
     }
     @Override
     public int eval(HashMap<String, Integer> identifier) throws EvalException {
-        return 0;
+        int lv = left.eval(identifier);
+        int rv = right.eval(identifier);
+        if (op.equals("+")) return lv + rv;
+        if (op.equals("-")) return lv - rv;
+        if (op.equals("*")) return lv * rv;
+        if (op.equals("/")) return lv / rv;
+        if (op.equals("%")) return lv % rv;
+        if (op.equals("^")) return (int) Math.pow(lv,rv);
+        throw new EvalException("unknown operator: " + op);
     }
 
     @Override
     public void prettyPrint(StringBuilder s) {
-
+        s.append("(");
+        left.prettyPrint(s);
+        s.append(op);
+        right.prettyPrint(s);
+        s.append(")");
     }
 }
