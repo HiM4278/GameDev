afe25c6d1baeb6d71e68b2791fa7af5a886a9902
Atipat-CMU <atipat_d@cmu.ac.th>
2023-02-15 16:50:32 +0700

    remove all test in ExpressionParserTest.java

--- src/test/java/evaluator/parser/ExpressionParserTest.java
+++ src/test/java/evaluator/parser/ExpressionParserTest.java
@@ -11,16 +11,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ExpressionParserTest {
 
-    @Test
-    void main() throws SyntaxErrorException, EvalException {
-        PlanParser exprParser = new PlanParser();
-        Expression expression = exprParser.parse("1 + 3 - 4");
-        assertEquals(0,expression.eval(new HashMap<>()));
-
-        expression = exprParser.parse("2 * 2 ^ 2-1");
-        assertEquals(4,expression.eval(new HashMap<>()));
-
-        expression = exprParser.parse("2 * 2 ^ 3 ^ 2");
-        assertEquals(1024,expression.eval(new HashMap<>()));
-    }
 }
