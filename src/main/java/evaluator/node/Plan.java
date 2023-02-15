e1281d9902d10a28d873d6cfa95d6bf799fdba93
Atipat-CMU <atipat_d@cmu.ac.th>
2023-02-15 16:52:20 +0700

    Plan.prettyPrint completed

--- src/main/java/evaluator/node/Plan.java
+++ src/main/java/evaluator/node/Plan.java
@@ -1,10 +1,25 @@
 package evaluator.node;
 
-import evaluator.node.Node;
+import evaluator.node.statement.Statement;
+
+import java.util.LinkedList;
 
 public class Plan implements Node {
-    @Override
-    public void prettyPrint(StringBuilder s) {
+    private final LinkedList<Statement> statements;
 
+    public Plan() {
+        this.statements = new LinkedList<>();
+    }
+
+    @Override
+    public void prettyPrint(StringBuilder s, int depth) {
+        for(Statement statement: statements) {
+            statement.prettyPrint(s, depth+1);
+            s.append("\n");
+        }
+    }
+
+    public void append(Statement statement){
+        statements.add(statement);
     }
 }
