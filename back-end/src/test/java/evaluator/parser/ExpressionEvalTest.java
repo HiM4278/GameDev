44465cd0b3920d3234231fd82df8f956e4e0314f
Raiwin Inthasit <andaraiwin@gmail.com>
2023-02-22 19:45:22 +0700

    Add excessive Parentheses testing case, should be thrown.

--- back-end/src/test/java/evaluator/parser/ExpressionEvalTest.java
+++ back-end/src/test/java/evaluator/parser/ExpressionEvalTest.java
@@ -4,7 +4,6 @@ import evaluator.node.expression.Expression;
 import evaluator.tokenizer.PlanTokenizer;
 import exeption.EvalException;
 import exeption.SyntaxErrorException;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -20,77 +19,77 @@ public class ExpressionEvalTest {
         public void numberTest () throws SyntaxErrorException, EvalException {
             PlanTokenizer tkz = new PlanTokenizer();
             tkz.updateSource("069");
-            ExprParser psr = new ExprParser(tkz);
-            Expression expr = psr.parse();
+            PlanParser psr = new PlanParser(tkz);
+            Expression expr = psr.parseExprForTest();
             assertEquals(69, expr.eval(null));
         }
         @Test
         public void AdditionTest () throws SyntaxErrorException, EvalException {
             PlanTokenizer tkz = new PlanTokenizer();
             tkz.updateSource("33+36");
-            ExprParser psr = new ExprParser(tkz);
-            Expression expr = psr.parse();
+            PlanParser psr = new PlanParser(tkz);
+            Expression expr = psr.parseExprForTest();
             assertEquals(69, expr.eval(null));
         }
         @Test
         public void SubtractionTest () throws SyntaxErrorException, EvalException {
             PlanTokenizer tkz = new PlanTokenizer();
             tkz.updateSource("100 - 31");
-            ExprParser psr = new ExprParser(tkz);
-            Expression expr = psr.parse();
+            PlanParser psr = new PlanParser(tkz);
+            Expression expr = psr.parseExprForTest();
             assertEquals(69, expr.eval(null));
         }
         @Test
         public void MultiplicationTest () throws SyntaxErrorException, EvalException {
             PlanTokenizer tkz = new PlanTokenizer();
             tkz.updateSource("23*3");
-            ExprParser psr = new ExprParser(tkz);
-            Expression expr = psr.parse();
+            PlanParser psr = new PlanParser(tkz);
+            Expression expr = psr.parseExprForTest();
             assertEquals(69, expr.eval(null));
         }
         @Test
         public void DivisionTest () throws SyntaxErrorException, EvalException {
             PlanTokenizer tkz = new PlanTokenizer();
             tkz.updateSource("138/2");
-            ExprParser psr = new ExprParser(tkz);
-            Expression expr = psr.parse();
+            PlanParser psr = new PlanParser(tkz);
+            Expression expr = psr.parseExprForTest();
             assertEquals(69, expr.eval(null));
         }
         @Test
         public void ModuloTest () throws SyntaxErrorException, EvalException {
             PlanTokenizer tkz = new PlanTokenizer();
-            tkz.updateSource("100 % 31");
-            ExprParser psr = new ExprParser(tkz);
-            Expression expr = psr.parse();
+            tkz.updateSource("31 % 100");
+            PlanParser psr = new PlanParser(tkz);
+            Expression expr = psr.parseExprForTest();
             assertEquals(69, expr.eval(null));
         }
         @Test
         public void powerTest () throws SyntaxErrorException, EvalException {
             PlanTokenizer tkz = new PlanTokenizer();
             tkz.updateSource("13^2");
-            ExprParser psr = new ExprParser(tkz);
-            Expression expr = psr.parse();
+            PlanParser psr = new PlanParser(tkz);
+            Expression expr = psr.parseExprForTest();
             assertEquals(169, expr.eval(null));
         }
         @Test
         public void PEMDASTest () throws SyntaxErrorException, EvalException {
             PlanTokenizer tkz = new PlanTokenizer();
             tkz.updateSource("(a+b-c) *x /y %z + xyz");
-            ExprParser psr = new ExprParser(tkz);
-            Expression expr = psr.parse();
+            PlanParser psr = new PlanParser(tkz);
+            Expression expr = psr.parseExprForTest();
             assertEquals(69,expr.eval(Map.of("a",10,"b",5,"c",1,"x",5,"y",1,"z",100, "xyz", -1)));
         }
         @Test
         public void associativityTest() throws SyntaxErrorException, EvalException{
             PlanTokenizer tkz = new PlanTokenizer();
             tkz.updateSource("333 * 222 / 111");
-            ExprParser psr = new ExprParser(tkz);
-            Expression expr = psr.parse();
+            PlanParser psr = new PlanParser(tkz);
+            Expression expr = psr.parseExprForTest();
 
             PlanTokenizer tkz2 = new PlanTokenizer();
             tkz2.updateSource("222 / 111 * 333");
-            ExprParser psr2 = new ExprParser(tkz2);
-            Expression expr2 = psr2.parse();
+            PlanParser psr2 = new PlanParser(tkz2);
+            Expression expr2 = psr2.parseExprForTest();
 
             assertEquals(666, expr.eval(null));
             assertEquals(666, expr2.eval(null));
@@ -99,17 +98,19 @@ public class ExpressionEvalTest {
 
     @Nested
     public class ArithmeticFailTest {
-        @Disabled
+
         @Test
         public void ParenthesesFailTest () throws SyntaxErrorException {
             PlanTokenizer tkz = new PlanTokenizer();
             tkz.updateSource("((333)))");
-            ExprParser psr = new ExprParser(tkz);
-//            Expression expr = psr.parse();
+            PlanParser psr = new PlanParser(tkz);
 
-//            PlanParser psr = new PlanParser(tkz);
+            PlanTokenizer tkz2 = new PlanTokenizer();
+            tkz2.updateSource("((333)");
+            PlanParser psr2 = new PlanParser(tkz2);
 
             assertThrows(SyntaxErrorException.class, psr::parse);
+            assertThrows(SyntaxErrorException.class, psr2::parse);
         }
     }
 
