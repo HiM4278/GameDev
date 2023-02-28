a9a46ca01b0cf69979875daaa540efcac74a4d0b
pasakorn <pasakorn141@gmail.com>
2023-02-28 14:06:41 +0700

    Land 1st Version

--- back-end/src/main/java/game/object/Land.java
+++ back-end/src/main/java/game/object/Land.java
@@ -4,11 +4,14 @@ import game.main.Player;
 import game.main.Region;
 
 public class Land implements Realty{
-    protected Region position;
+    private Region position;
+    private boolean isCityCenter;
     private Player Owner;
-    protected double deposit;
-    public Land(double deposit,Player owner,Region region){
-        this.deposit = deposit;
+    private long maxDep;
+    private double deposit;
+    public Land(long maxDeposit,Player owner,Region region){
+        this.maxDep = maxDeposit;
+        this.deposit = maxDeposit;
         this.Owner = owner;
         this.position = region;
     }
@@ -27,11 +30,30 @@ public class Land implements Realty{
 
     @Override
     public boolean collect(int money) {
-        return false;
+        if (deposit < money){
+            return false;
+        } else {
+           double d =  deposit -= money;
+           deposit = d;
+            if(d < 0){
+                deposit = 0;
+            }
+            return true;
+        }
     }
 
     @Override
     public boolean invest(int budget) {
+        double d = deposit + budget;
+        if (d > maxDep){
+            deposit = maxDep;
+        }
         return false;
+    }
+    public Player getOwner(){
+        return Owner;
+    }
+    public void setThisToCenter(){
+
     }
 }
