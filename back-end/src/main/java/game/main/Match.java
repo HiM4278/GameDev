720816616a9d414b731b390eaae20c1f41b052dc
pasakorn <pasakorn141@gmail.com>
2023-02-28 14:01:57 +0700

    Create class Match

--- back-end/src/main/java/game/main/Match.java
+++ back-end/src/main/java/game/main/Match.java
@@ -1,4 +1,27 @@
 package game.main;
 
+import extra.LoopCounter;
+
+import java.util.ArrayList;
+
 public class Match {
+    private int Turn;
+    public int numPlayer;
+    private Player host;
+    private ArrayList<Player> player;
+    private Territory territory;
+    private Configuration configuration;
+    private LoopCounter currPlayID;
+    public Match(int maxPlayer){
+
+    }
+    private void appendPlayer(){
+
+    }
+    public void start(){
+
+    }
+    public void nextPlayer(){
+
+    }
 }
