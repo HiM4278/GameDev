9d5e2cbde88b69abbb60a3b4f05a60f36c1205a6
pasakorn <pasakorn141@gmail.com>
2023-03-05 15:36:12 +0700

    CityCrew is finish

--- back-end/src/main/java/game/object/CityCrew.java
+++ back-end/src/main/java/game/object/CityCrew.java
@@ -3,6 +3,8 @@ package game.object;
 import extra.Direction;
 import game.main.Region;
 
+import static java.lang.Math.max;
+
 public class CityCrew implements Unit{
     private Region position;
     private int Budget;
@@ -19,9 +21,11 @@ public class CityCrew implements Unit{
 
     @Override
     public boolean move(Direction d) {
+        Budget -= 1;
+        checkBudget();
         if (Budget < 1 ) return false;
         else {
-            Budget -= 1;
+            position.getUnit().move(d);
             return true;
         }
     }
@@ -29,19 +33,44 @@ public class CityCrew implements Unit{
         return Budget;
     }
     public boolean relocate(){
+        Budget -= 1;
+        checkBudget();
         int val = 5 * position.getX() + 10;
         if(Budget >= val ) return false;
         return true;
     }
     public boolean shoot(Direction direction,long value){
         Budget -= 1;
+        checkBudget();
         if(Budget < value) return false;
         else {
-            Budget -= value;
+            if (position.getNeighbor(direction) != null){
+                Region r = position.getNeighbor(direction);
+                Budget -= value;
+                r.getRealty().increase(value);
+            }
             return true;
         }
     }
     public void invest(long value){
+        Budget -= 1;
+        checkBudget();
+        if (Budget < value){}
+        else{ position.getRealty().decrease(value); }
 
+    }
+    public boolean collect(long value){
+        Budget -= 1;
+        checkBudget();
+        if (position.getRealty().increase(value)){
+            Budget += value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void checkBudget(){
+        this.Budget = max(Budget,0);
     }
 }
