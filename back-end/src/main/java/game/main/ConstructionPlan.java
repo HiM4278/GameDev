1c740821702c6eb9f5d9d5214bb391a86c9bec91
Raiwin Inthasit <andaraiwin@gmail.com>
2023-03-13 12:04:13 +0700

    update ConstructionPlan

--- back-end/src/main/java/game/main/ConstructionPlan.java
+++ back-end/src/main/java/game/main/ConstructionPlan.java
@@ -2,6 +2,7 @@ package game.main;
 
 import evaluator.node.Plan;
 import evaluator.parser.PlanParser;
+import evaluator.tokenizer.PlanTokenizer;
 import exeption.SyntaxErrorException;
 
 import java.util.HashMap;
@@ -10,27 +11,25 @@ public class ConstructionPlan {
     private Player owner;
     private String strPlan;
     private PlanParser plan;
-    private HashMap<String, Integer> identifiers;
+    private PlanTokenizer tokenizer = new PlanTokenizer();
+    private HashMap<String, Integer> identifiers = new HashMap<>();
 
     public ConstructionPlan(Player owner){
         this.owner = owner;
     }
 
-    public void updatePlan(String src){
+    public void updatePlan(String src) throws SyntaxErrorException {
         strPlan = src;
+        tokenizer.updateSource(strPlan);
+    }
+
+    public void run() throws SyntaxErrorException {
         parsePlan();
     }
 
-    public void run(){
-
-    }
-
-    private void parsePlan(){
-//        try {
-//            plan = plan.parse(strPlan);
-//        } catch (SyntaxErrorException e){
-//            System.out.println(e);
-//        }
+    private void parsePlan() throws SyntaxErrorException {
+        plan = new PlanParser(tokenizer);
+        plan.parse();
     }
 
 
